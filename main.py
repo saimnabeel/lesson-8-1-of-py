5007ce4599c042c4a4e6d4dbdc9dcc9bb3685e5b
saimnabeel <Saimnabeeliqbal@gmail.com>
2025-11-21 17:46:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+class Saim:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        name="Saim"
+        age=25
+        gender="Male"
+    def introduce(self):
+        return f"Hello, my name is {self.name}, I am {self.age} years old and I am a {self.gender}."
+    
+    
